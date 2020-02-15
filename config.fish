adcc71d091e66e09b9cf129fdcd75519c9c8c2b8
Jesus Gonzalez <jesus@jesusmg.net>
2020-02-14 22:38:50 -0300

    Fixed setup.fish symlink creation. Added config file for fish.

---
+++ config.fish
@@ -0,0 +1,3 @@
+powerline-daemon -q
+set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+powerline-setup
--- setup.fish
+++ setup.fish
@@ -1,6 +1,10 @@
 #!/usr/bin/fish
 
-mkdir -p ~/.config/nvim
-ln -sf init.vim ~/.config/nvim/
+mkdir -p $HOME/.config/nvim
+ln -rsf ./init.vim $HOME/.config/nvim/init.vim
 
-ln -sf .tmux.conf ~/
+ln -rsf ./.tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/.config/fish
+ln -rsf ./config.fish $HOME/.config/fish/config.fish
+
